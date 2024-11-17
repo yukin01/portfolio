5f8a2a658614478ac3e46da0898ccdeeb23cb26a
yukin01 <38382781+yukin01@users.noreply.github.com>
2024-11-17 19:20:36 +0900

    feat: add zenn link and update styles

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -10,21 +10,24 @@ type Site = {
 const Profile: FC = () => {
   const sites: Site[] = [
     { name: 'GitHub', url: 'https://github.com/yukin01' },
-    { name: 'Twitter', url: 'https://twitter.com/_yukin01' },
+    { name: 'X (Twitter)', url: 'https://x.com/_yukin01' },
     { name: 'SpeakerDeck', url: 'https://speakerdeck.com/yukin01' },
+    { name: 'Zenn', url: 'https://zenn.dev/yukin01' },
     { name: 'Qiita', url: 'https://qiita.com/yukin01' }
   ]
   return (
     <div>
-      <Image src={profile} alt="Profile image" />
-      <p>Kinjo Yuji (yukin01)</p>
-      {sites.map((site) => (
-        <div key={site.name}>
-          <a href={site.url} target="_blank" rel="noreferrer noopener">
-            {site.name}
-          </a>
-        </div>
-      ))}
+      <Image src={profile} alt="Profile image" width={240} height={240} style={{ borderRadius: '120px' }} />
+      <h2>Kinjo Yuji (yukin01)</h2>
+      <ul>
+        {sites.map((site) => (
+          <li key={site.name} style={{ marginBottom: '5px' }}>
+            <a href={site.url} target="_blank" rel="noreferrer noopener">
+              {site.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
