c47a471b0a0a93d3913e3b340e1d920b4d8e8dda
yukin01 <38382781+yukin01@users.noreply.github.com>
2024-11-20 02:37:20 +0900

    feat: use icon

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,29 +1,48 @@
 import { FC } from 'react'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import profile from '../public/profile.png'
+
+import x from '../public/icons/x.png'
+import github from '../public/icons/github.png'
+import speakerDeck from '../public/icons/speaker-deck.png'
+import zenn from '../public/icons/zenn.svg'
+import qiita from '../public/icons/qiita.png'
 
 type Site = {
   name: string
   url: string
+  icon: StaticImageData
+  size: number
 }
 
 const Profile: FC = () => {
   const sites: Site[] = [
-    { name: 'GitHub', url: 'https://github.com/yukin01' },
-    { name: 'X (Twitter)', url: 'https://x.com/_yukin01' },
-    { name: 'SpeakerDeck', url: 'https://speakerdeck.com/yukin01' },
-    { name: 'Zenn', url: 'https://zenn.dev/yukin01' },
-    { name: 'Qiita', url: 'https://qiita.com/yukin01' }
+    { name: 'GitHub', url: 'https://github.com/yukin01', icon: github, size: 24 },
+    { name: 'X (Twitter)', url: 'https://x.com/_yukin01', icon: x, size: 24 },
+    { name: 'SpeakerDeck', url: 'https://speakerdeck.com/yukin01', icon: speakerDeck, size: 32 },
+    { name: 'Zenn', url: 'https://zenn.dev/yukin01', icon: zenn, size: 24 },
+    { name: 'Qiita', url: 'https://qiita.com/yukin01', icon: qiita, size: 24 }
   ]
   return (
     <div>
       <Image src={profile} alt="Profile image" width={240} height={240} style={{ borderRadius: '120px' }} />
-      <h2>Kinjo Yuji - @yukin01</h2>
-      <ul>
+      <h2 style={{ textAlign: 'center' }}>Kinjo Yuji - @yukin01</h2>
+      <p style={{ textAlign: 'center' }}>SRE / Platform Engineer</p>
+      <ul style={{ display: 'flex', justifyContent: 'center', listStyleType: 'none', paddingLeft: 0 }}>
         {sites.map((site) => (
-          <li key={site.name} style={{ marginBottom: '5px' }}>
+          <li
+            key={site.name}
+            style={{
+              width: '32px',
+              height: '32px',
+              margin: '4px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
             <a href={site.url} target="_blank" rel="noreferrer noopener">
-              {site.name}
+              <Image src={site.icon} alt={site.name} width={site.size} height={site.size} unoptimized />
             </a>
           </li>
         ))}
